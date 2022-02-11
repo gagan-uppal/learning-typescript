cdbb5d3cf4b5784bc7f56253bfd95badb9dfac0a
Gagandeep Uppal <guppal.profile@gmail.com>
2022-02-11 14:41:43 -0500

    updated

--- index.ts
+++ index.ts
@@ -12,23 +12,23 @@
   let realAge = 0; //the type is number, so it has to be number
   
   if (aged) {
-    realAge = 4;
+    realAge = 5;
   }
   
   let dogAge = realAge * 7;
   
   console.log(`${dogAge} years`);
 
-  ////////// Type Shapes //////////
+  // Type Shapes //
 
 // An object’s shape describes, among other things, what properties and methods it does or doesn’t contain.
-let firstName = 'muriel!';
+let firstName = 'gagan!';
 
 console.log(firstName.toUpperCase());
 
 console.log(firstName.length);
 
-///////// Type Any //////////
+/// Type Any //
 
 // Variables of type any can be assigned to any value and TypeScript won’t give an error if they’re reassigned to a different type later on.
 
@@ -36,3 +36,16 @@ let guess; // TypeScript considers it to be of type any
 
 guess = 'guess who'
 guess = 2;
+
+// Variable Type Annotations //
+
+// Variables can have type annotations (also known as type declarations) added just after their names. 
+// Provide a type annotation by appending a variable with a colon (:) and the type (e.g., number, string, or any).
+
+let phoneNumber : string;
+
+if (Math.random() > 0.5) {
+  phoneNumber = '123456789';
+} else {
+  phoneNumber = '987654321';
+}

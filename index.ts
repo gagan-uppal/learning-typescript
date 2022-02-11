16e9d7a6a3b98437fb879f7d7a1befd1660c0b1f
Gagandeep Uppal <guppal.profile@gmail.com>
2022-02-11 14:31:40 -0500

    index file

---
+++ index.ts
@@ -0,0 +1,20 @@
+// Type Inferences
+
+// TypeScript expects the data type of the variable to match the type of the value initially assigned to it at declaration.
+// TypeScript recognizes JavaScript’s built-in “primitive” data types:  
+  // boolean
+  // number
+  // null
+  // string
+  // undefined
+
+  let aged = true;
+  let realAge = 0; //the type is number, so it has to be number
+  
+  if (aged) {
+    realAge = 4;
+  }
+  
+  let dogAge = realAge * 7;
+  
+  console.log(`${dogAge} years`);

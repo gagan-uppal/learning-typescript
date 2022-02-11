9d44c73aa6209788614b4c354a2f7023494e7e8c
Gagandeep Uppal <guppal.profile@gmail.com>
2022-02-11 14:34:02 -0500

    add type shapes

--- index.ts
+++ index.ts
@@ -18,3 +18,12 @@
   let dogAge = realAge * 7;
   
   console.log(`${dogAge} years`);
+
+  ////////// Type Shapes //////////
+
+// An object’s shape describes, among other things, what properties and methods it does or doesn’t contain.
+let firstName = 'muriel!';
+
+console.log(firstName.toUpperCase());
+
+console.log(firstName.length);

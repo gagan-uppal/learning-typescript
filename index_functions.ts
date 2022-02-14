f2d378a2ef13ee97ac7e8315202de7c1b99503a4
Gagandeep Uppal <guppal.profile@gmail.com>
2022-02-14 16:35:21 -0500

    fuctions

---
+++ index_functions.ts
@@ -0,0 +1,6 @@
+// Function parameters may be given type annotations with the same syntax as variable declarations: 
+// a colon next to the name
+
+function triple(value:number) {
+  return value * 3;
+}
